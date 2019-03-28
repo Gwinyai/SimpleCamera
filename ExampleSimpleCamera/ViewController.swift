f9932b560dfd1a5647ef77497922581534421ade
Gwinyai <gnyatsoka@gmail.com>
2019-03-28 11:53:42 +0200

    Initial Commit

---
+++ ExampleSimpleCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleSimpleCamera
+//
+//  Created by Gwinyai on 28/3/2019.
+//  Copyright © 2019 Gwinyai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
